f91ee9d955961d97953ab45bb614986c805f4318
Kyohey Sekiya <Kyohey.sekiya@gmail.com>
2020-09-16 16:21:46 +0900

    ユーザー登録実装（仮）

--- app/controllers/musics_controller.rb
+++ app/controllers/musics_controller.rb
@@ -1,5 +1,24 @@
 class MusicsController < ApplicationController
+  before_action :set_music, only: [:edit, :show]
+  before_action :move_to_index, except: [:index]
+
+
   def index
     @musics = Music.all
+  end
+
+  private
+  def music_params
+    # params.require(:music).permit(:image, :title, :artist)
+  end
+
+  def set_music
+    @music = Music.find(params[:id])
+  end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
   end
 end
