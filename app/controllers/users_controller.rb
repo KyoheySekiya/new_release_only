5c1d29170bfeb1eabebffac43c2113be72212707
Kyohey Sekiya <Kyohey.sekiya@gmail.com>
2020-09-19 00:28:49 +0900

    詳細・編集・削除機能実装確認済み

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,37 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :show, :update, :destroy]
+
   def show
-    user = User.find(params[:id])
-    @nickname = user.nickname
-    @musics = user.musics
+    @musics = @user.musics
   end
+
+  def edit
+    
+  end
+
+  def update
+    @musics = @user.musics
+    if @user.update(user_params)
+      render :show
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    if @user.destroy
+      redirect_to root_path
+    else
+      render :show
+    end
+  end
+  private
+  def user_params
+    params.require(:user).permit(:nickname, :family_name, :first_name, :family_name_kana, :first_name_kana, :introduction, :id)
+  end
+  
+  def set_user
+    @user = User.find(params[:id])
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to:'musics#index'
   resources :musics
-  resources :users, only: :show
+  resources :users, only: [:show, :edit, :update, :destroy]
 end
