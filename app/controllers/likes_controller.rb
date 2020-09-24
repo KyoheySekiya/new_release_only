fe09c59444f0dfcec0951e4b6aa7c841317c9a3b
Kyohey Sekiya <Kyohey.sekiya@gmail.com>
2020-09-24 16:21:21 +0900

    ajax学習のため中断

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,13 +2,13 @@ class LikesController < ApplicationController
   before_action :set_like
   def create
     Like.create(user_id: current_user.id, music_id: @music.id)
-    redirect_to music_path(@music.id)
+    # redirect_to music_path(@music.id)
 
   end
 
   def destroy
     Like.find_by(user_id: current_user.id, music_id: @music.id).destroy
-    redirect_to music_path(@music.id)
+    # redirect_to music_path(@music.id)
   end
 
   private
